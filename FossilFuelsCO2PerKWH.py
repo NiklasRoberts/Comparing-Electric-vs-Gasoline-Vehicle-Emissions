6bd15104967dfe58703c22f794141b87b9ebe717
Niklas Roberts <niklasroberts@gmail.com>
2022-11-13 21:20:12 -0800

    Separate lbs_per_kwh data retrieval for fossil fuels

---
+++ FossilFuelsCO2PerKWH.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+electricity_and_co2_by_source = pd.read_csv("Data/PowerAndCO2EmissionsBySource.csv")
+lbs_per_kwh = {}
+
+
+coal = electricity_and_co2_by_source.loc[electricity_and_co2_by_source["Sources"] == "Coal"]
+# Unit: Million kWh
+coal_electricity_generation = int(coal["Electricity Generation (million kWh)"])
+# Unit: Million US tons
+coal_CO2_produced = int(coal["CO2 Emissions (million US tons)"])
+# Unit: lbs/kWh
+lbs_per_kwh["Coal"] = round((coal_CO2_produced*2000)/coal_electricity_generation, 3)
+
+
+nat_gas = electricity_and_co2_by_source.loc[electricity_and_co2_by_source["Sources"] == "Natural Gas"]
+# Unit: Million kWh
+nat_gas_electricity_generation = int(nat_gas["Electricity Generation (million kWh)"])
+# Unit: Million US tons
+nat_gas_CO2_produced = int(nat_gas["CO2 Emissions (million US tons)"])
+# Unit: lbs/kWh
+lbs_per_kwh["Natural Gas"] = round((nat_gas_CO2_produced*2000)/nat_gas_electricity_generation, 3)
+
+
+petroleum = electricity_and_co2_by_source.loc[electricity_and_co2_by_source["Sources"] == "Petroleum"]
+# Unit: Million kWh
+petroleum_electricity_generation = int(petroleum["Electricity Generation (million kWh)"])
+# Unit: Million US tons
+petroleum_CO2_produced = int(petroleum["CO2 Emissions (million US tons)"])
+# Unit: lbs/kWh
+lbs_per_kwh["Petroleum"] = round((petroleum_CO2_produced*2000)/petroleum_electricity_generation, 3)
--- TotalCO2EmissionsFromEV.py
+++ TotalCO2EmissionsFromEV.py
@@ -9,47 +9,25 @@
 
 # Assumption: Electric Vehicles are charged using electricity generated from various power sources, so the CO2 emitted from electric vehicles is equivalent to the CO2 emitted from the average of all power sources used to generate the electricity
 import pandas as pd
+import FossilFuelsCO2PerKWH
+from copy import deepcopy
 
-electricity_and_co2_by_source = pd.read_csv("Data/PowerAndCO2EmissionsBySource.csv")
+### Prepare dictionary mapping power source: lbs CO2 emitted per kwh energy generated
+EV_lbs_per_kwh = deepcopy(FossilFuelsCO2PerKWH.lbs_per_kwh)
+
 # Renewable energy sources generate no CO2 emissions
 # Assumption: Nuclear generates no CO2, ignoring possible emissions during supplies extraction for simplicity
 # Assumption: The plants that are the source of biomass for energy capture almost the same amount of CO2 through photosynthesis while growing as is released when biomass is burned, which can make biomass a carbon-neutral energy source: https://www.eia.gov/energyexplained/biomass/biomass-and-the-environment.php
-lbs_per_kwh = {renewable: 0 for renewable in ["Nuclear", "Wind", "Hydropower", "Solar", "Biomass", "Geothermal"]}
+for renewable in ["Nuclear", "Wind", "Hydropower", "Solar", "Biomass", "Geothermal"]:
+    EV_lbs_per_kwh[renewable] = 0
 
-
-coal = electricity_and_co2_by_source.loc[electricity_and_co2_by_source["Sources"] == "Coal"]
-# Unit: Million kWh
-coal_electricity_generation = int(coal["Electricity Generation (million kWh)"])
-# Unit: Million US tons
-coal_CO2_produced = int(coal["CO2 Emissions (million US tons)"])
-# Unit: lbs/kWh
-lbs_per_kwh["Coal"] = round((coal_CO2_produced*2000)/coal_electricity_generation, 3)
-
-
-nat_gas = electricity_and_co2_by_source.loc[electricity_and_co2_by_source["Sources"] == "Natural Gas"]
-# Unit: Million kWh
-nat_gas_electricity_generation = int(nat_gas["Electricity Generation (million kWh)"])
-# Unit: Million US tons
-nat_gas_CO2_produced = int(nat_gas["CO2 Emissions (million US tons)"])
-# Unit: lbs/kWh
-lbs_per_kwh["Natural Gas"] = round((nat_gas_CO2_produced*2000)/nat_gas_electricity_generation, 3)
-
-
-petroleum = electricity_and_co2_by_source.loc[electricity_and_co2_by_source["Sources"] == "Petroleum"]
-# Unit: Million kWh
-petroleum_electricity_generation = int(petroleum["Electricity Generation (million kWh)"])
-# Unit: Million US tons
-petroleum_CO2_produced = int(petroleum["CO2 Emissions (million US tons)"])
-# Unit: lbs/kWh
-lbs_per_kwh["Petroleum"] = round((petroleum_CO2_produced*2000)/petroleum_electricity_generation, 3)
-
-
-# Assumption: "Various gases" generate same emissions as natural gases
-lbs_per_kwh["Other gases"] = lbs_per_kwh["Natural Gas"]
+# Assumption: "Other gases" generate same emissions as natural gases
+EV_lbs_per_kwh["Other gases"] = EV_lbs_per_kwh["Natural Gas"]
 # Assumption: "Other sources" generates emissions equivalent to the average of all other fossil fuel sources
-lbs_per_kwh["Other sources"] = sum([lbs_per_kwh[fossil_fuel] for fossil_fuel in ["Coal", "Natural Gas", "Petroleum"]])/3
+EV_lbs_per_kwh["Other sources"] = sum([EV_lbs_per_kwh[fossil_fuel] for fossil_fuel in ["Coal", "Natural Gas", "Petroleum"]])/3
 
 
+### Compute average CO2 emitted per kWh among all power sources
 electricity_generation_by_source = pd.read_csv("Data/PowerGenerationBySource.csv")
 fuel_sources = ["Natural Gas", "Coal", "Petroleum", "Other gases", "Nuclear", "Wind", "Hydropower", "Solar", "Biomass", "Geothermal", "Other sources"]
 percent_electricity_generation = {}
@@ -59,7 +37,7 @@ for fuel_source in fuel_sources:
 
 us_avg_lbs_per_kwh = 0
 for fuel_source in fuel_sources:
-    us_avg_lbs_per_kwh += lbs_per_kwh[fuel_source] * percent_electricity_generation[fuel_source]
+    us_avg_lbs_per_kwh += EV_lbs_per_kwh[fuel_source] * percent_electricity_generation[fuel_source]
 us_avg_lbs_per_kwh = round(us_avg_lbs_per_kwh, 3)
 
 
@@ -70,7 +48,7 @@ if __name__ == "__main__":
     print("\n")
 
     print("Fuel Source : CO2 (lbs) emitted in generating 1 kWh of power")
-    for fuel_source, lbs_kwh in lbs_per_kwh.items():
+    for fuel_source, lbs_kwh in EV_lbs_per_kwh.items():
         if lbs_kwh == 0:
             print(f"{fuel_source} : Carbon Neutral")
         else:
